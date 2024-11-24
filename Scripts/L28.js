bfa32caee5caac01dc06203fa677ce69e72c664e
Brtelfer <106611498+Brtelfer@users.noreply.github.com>
2024-11-23 18:49:17 -0600

    Update L28.js

--- Scripts/L28.js
+++ Scripts/L28.js
@@ -6,7 +6,7 @@ async function setTexts(slidenumber) {
         return;
     }
 
-    const url = 'https://raw.githubusercontent.com/Brtelfer/Brtelfer.github.io/Data/L28.json';
+    const url = 'https://raw.githubusercontent.com/Brtelfer/Brtelfer.github.io/refs/heads/main/Data/L28.json';
     let texts;
 
     try {
